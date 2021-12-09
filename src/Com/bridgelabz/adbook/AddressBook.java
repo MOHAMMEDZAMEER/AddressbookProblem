d7468dc1fa44aa62330e74c5fef216c464858b8b
MOHAMMEDZAMEER <zameer4939@gmail.com>
2021-12-09 02:37:54 -0400

    New Contact added to AddressBook

--- src/Com/bridgelabz/adbook/AddressBook.java
+++ src/Com/bridgelabz/adbook/AddressBook.java
@@ -1,11 +1,62 @@
 package Com.bridgelabz.adbook;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class AddressBook {
+	//Input the Contact Information
+	 Scanner Z = new Scanner(System.in);
+	
+	 ArrayList<ContactInfo> contactbook = new ArrayList<>();
+	//Creting addcontact method
+	public void addcontact() {
+		System.out.print("Enter FirstName: ");
+		String First= Z.next();
+	
+		System.out.print("Enter LastName: ");
+		String Last = Z.next();
+		
+		System.out.print("Enter Address: ");
+		String Address= Z.next();
+		
+		System.out.print("Enter City: ");
+		String City = Z.next();
+		
+		System.out.print("Enter State: ");
+		String State = Z.next();
+		
+		System.out.print("Enter ZipCode: ");
+		int Zip = Z.nextInt();
+		
+		System.out.print("Enter PhoneNumber: ");
+		int Phone = Z.nextInt();
+		
+		System.out.print("Enter Email: ");
+		String email = Z.next();
+	
+	ContactInfo contact = new ContactInfo(First, Last, Address, City, State, Zip, Phone, email);
+			contactbook.add(contact);
+	System.out.println("\n Contact Added SuccsessFully");
+	
+	}
+	// creating Diplay Method
+	public void Displaycontact() {
+		System.out.println("\n --------Contacts in AddressBook-------");
+		for (ContactInfo person : contactbook) {
+			System.out.println(person.toString());
+		}
+	}
+	
+	
 
 	/*Main Method*/
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("---------WELLCOME TO ADDRESSBOOK PROBLEM---------");
+	AddressBook obj = new AddressBook();
+	obj.addcontact();
+	obj.Displaycontact();
 	}
+	
 
 }
---
+++ src/Com/bridgelabz/adbook/ContactInfo.java
@@ -0,0 +1,131 @@
+package Com.bridgelabz.adbook;
+
+/*Contact class contains Variables*/
+public class ContactInfo {
+
+	String FirstName;
+	String LastName;
+	String Address;
+	String City;
+	String State;
+	int ZipCode;
+	int PhoneNum;
+	String Email;
+	
+	public ContactInfo(String fn,String ls,String a,String c,String s,int z,int pn,String e) {
+		
+		this .FirstName=fn;
+		this.LastName=ls;
+		this.Address=a;
+		this.City=c;
+		this.State=s;
+		this.ZipCode=z;
+		this.PhoneNum=pn;
+		this.Email=e;
+	
+		
+	}
+	
+	/**Initiating Getter and Setter Me  thod**/
+	
+	public String getFirstName() {
+		return FirstName;
+	}
+
+
+
+	public void setFirstName(String firstName) {
+		FirstName = firstName;
+	}
+
+
+
+	public String getLastName() {
+		return LastName;
+	}
+
+
+
+	public void setLastName(String lastName) {
+		LastName = lastName;
+	}
+
+
+
+	public String getAddress() {
+		return Address;
+	}
+
+
+
+	public void setAddress(String address) {
+		Address = address;
+	}
+
+
+
+	public String getCity() {
+		return City;
+	}
+
+
+
+	public void setCity(String city) {
+		City = city;
+	}
+
+
+
+	public String getState() {
+		return State;
+	}
+
+
+
+	public void setState(String state) {
+		State = state;
+	}
+
+
+
+	public int getZipCode() {
+		return ZipCode;
+	}
+
+
+
+	public void setZipCode(int zipCode) {
+		ZipCode = zipCode;
+	}
+
+
+
+	public int getPhoneNum() {
+		return PhoneNum;
+	}
+
+
+
+	public void setPhoneNum(int phoneNum) {
+		PhoneNum = phoneNum;
+	}
+
+
+
+	public String getEmail() {
+		return Email;
+	}
+
+
+
+	public void setEmail(String email) {
+		Email = email;
+	}
+
+	//override Method
+	@Override
+	public String toString() {
+		return String.format(FirstName + " " +LastName + " " +Address +" " +City + " " +State + "" + ZipCode + " " +PhoneNum + " " +Email + " ");
+	}
+		
+	}

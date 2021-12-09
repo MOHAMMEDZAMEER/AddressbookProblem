23c0332df9765e0f13a49cfbe5d20a35e22228a1
MOHAMMEDZAMEER <zameer4939@gmail.com>
2021-12-09 01:46:13 -0400

    Wellcome message as return

---
+++ src/Com/bridgelabz/adbook/AddressBook.java
@@ -0,0 +1,10 @@
+package Com.bridgelabz.adbook;
+
+public class AddressBook {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("---------WELLCOME TO ADDRESSBOOK PROBLEM---------");
+	}
+
+}
